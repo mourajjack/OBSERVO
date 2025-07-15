b4488d67badefd6d87e9c8a48c98134d0a9250aa
J. Jackson Moura <developersjavi@gmail.com>
2025-07-14 21:11:12 -0300

    Add: Icon and SplashScreen

---
+++ OBSERVO/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OBSERVO
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Nome do Meu Projeto
+
+Descrição breve do projeto.
+
+## Como rodar
+
+Instruções para executar.
+
+## Tecnologias usadas
+
+- .NET MAUI
+- C#
+- ...
+
+## Autor
+
+Seu nome
