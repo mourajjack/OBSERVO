0784c7c673448bc7a107a8d84f8b28d1ca26ee94
J. Jackson Moura <developersjavi@gmail.com>
2025-07-20 13:44:47 -0300

    Add Companies Logo

---
+++ OBSERVO/Services/API_DB_CONN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBSERVO
+{
+    public static class API_DB_CONN
+    {
+        public static string URI_SelectCompany { get; set; } = @"https://script.google.com/macros/s/AKfycbwkAvRZ8044LkgCiJZ8ZoSxJ9tL4pKrH7o5-CuNqSJqx8emLawOlJ9mFtuBaY4PdfDKxQ/exec";
+        //public static string URI_RONDA { get; set; } = @"https://script.google.com/macros/s/AKfycbzJ4twcydKE2CRVyMQ10v2iHz3GVd6ZowjIRHGbpvOi83khMVgZJApq41MtXQgoUR8j1A/exec";
+    }
+}
--- OBSERVO/Views/SelectCompany.xaml.cs
+++ OBSERVO/Views/SelectCompany.xaml.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace OBSERVO.Views;
 
 public partial class SelectCompany : ContentPage
@@ -6,4 +8,59 @@ public partial class SelectCompany : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    public async Task<string> GetCompanyByName(string name)
+    {
+        try
+            {
+            HttpClient _httpClient = GetClient();
+
+            var response = await _httpClient.GetAsync(API_DB_CONN.URI_SelectCompany + "?name=" + name);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Wi-Fi erro. Verifique sua conexão com a internet e tente novamente mais tarde!");
+            }
+
+            var dataString = await response.Content.ReadAsStringAsync();
+
+            if (!string.IsNullOrEmpty(dataString))
+            {
+                var json = System.Text.Json.JsonDocument.Parse(dataString);
+                if (json.RootElement.GetProperty("success").GetBoolean())
+                {
+                    var logo = json.RootElement.GetProperty("logo").GetString();
+                    return logo;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                throw new Exception("Resposta vazia da API");
+            }
+
+        }
+        catch (Exception)
+        {
+            throw new Exception("INTERNET ERRO");
+        }
+    }
+
+    public HttpClient GetClient()
+    {
+        HttpClient client = new HttpClient();
+        client.Timeout = TimeSpan.FromSeconds(10);
+        client.DefaultRequestHeaders.Add("Accept", "Application/json");
+        client.DefaultRequestHeaders.Add("Connection", "close");
+        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        return client;
+    }
+
+    private async void OnPROSSEGUIR_Clicked(object sender, EventArgs e)
+    {
+        var logo = await GetCompanyByName("CETSEG");
+        MinhaImagem.Source = ImageSource.FromUri(new Uri(logo));
+    }
 }
