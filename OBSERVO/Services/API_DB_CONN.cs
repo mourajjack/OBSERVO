1b539d5d356383a0624096563e46ed3457c6ffd4
J. Jackson Moura <developersjavi@gmail.com>
2025-07-21 23:27:14 -0300

    acessar uma planilha pelo ID

--- OBSERVO/Services/API_DB_CONN.cs
+++ OBSERVO/Services/API_DB_CONN.cs
@@ -6,7 +6,7 @@ namespace OBSERVO
 {
     public static class API_DB_CONN
     {
-        public static string URI_SelectCompany { get; set; } = @"https://script.google.com/macros/s/AKfycbwkAvRZ8044LkgCiJZ8ZoSxJ9tL4pKrH7o5-CuNqSJqx8emLawOlJ9mFtuBaY4PdfDKxQ/exec";
+        public static string URI_SelectCompany { get; set; } = @"https://script.google.com/macros/s/AKfycbybpBK7UvEoY23vQXkZ9vM--OeKRw5F_2KsCeUTiMpPZ2KrJOtEPBVWnV3EfQCTbI_C0w/exec";
         //public static string URI_RONDA { get; set; } = @"https://script.google.com/macros/s/AKfycbzJ4twcydKE2CRVyMQ10v2iHz3GVd6ZowjIRHGbpvOi83khMVgZJApq41MtXQgoUR8j1A/exec";
     }
 }
