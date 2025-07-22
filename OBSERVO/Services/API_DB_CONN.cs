3feaa331698f5ca1e00cf3446ef31d41b73895cf
J. Jackson Moura <developersjavi@gmail.com>
2025-07-22 20:43:15 -0300

    funcionando

--- OBSERVO/Services/API_DB_CONN.cs
+++ OBSERVO/Services/API_DB_CONN.cs
@@ -7,6 +7,6 @@ namespace OBSERVO
     public static class API_DB_CONN
     {
         public static string URI_SelectCompany { get; set; } = @"https://script.google.com/macros/s/AKfycby73nD9tiMvUfw-Vop0BPurcsk7vzOv9niyQHsCgQ7P62Pp-_Zddl-ipfxnTzhiI_42ZQ/exec";
-        public static string URI_Colaboradores { get; set; } = @"https://script.google.com/macros/s/AKfycbwOggwGRkb3dOegRCDOhRIc1HMhWPICDhUHRtjI7zHvNGpgIKo7eyKzSrAcXcLI2iRczQ/exec";
+        public static string URI_Colaboradores { get; set; } = @"https://script.google.com/macros/s/AKfycbwOrdkO5vaNJ3AJknafoO4tLv7Yr4_lddBZxMKcVElTP-0YNhkPzrvctlsTj5tfcHGy4g/exec";
     }
 }
